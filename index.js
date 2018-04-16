1c2d8a3e914b2afed92a0fe16333dae6c8ec4da5
TranTony <tranthanhtrung1990@gmail.com>
2018-04-16 16:04:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    var remoteAddress = req.headers['x-forwarded-for'] || 
-    				  req.connection.remoteAddress;
-    var ipList = remoteAddress.split(",");
-    var lastProxy = ipList[ipList.length - 1].trim();
+    res.json(req);
     res.json({ "ipAddress": lastProxy });
 });
 
